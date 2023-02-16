0193036f6ea0514b8e7b6b03ab9b86de43a19a1d
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2023-02-16 10:55:46 +0100

    Post/list

--- ApiServerTE20B/Program.cs
+++ ApiServerTE20B/Program.cs
@@ -3,10 +3,20 @@ WebApplication app = WebApplication.Create();
 app.Urls.Add("http://localhost:3000");
 app.Urls.Add("http://*:3000");
 
-Cheese dairy = new Cheese();
-dairy.Kind = "Fourme d'Ambert";
-dairy.Taste = "Soft and creamy, milky with earthy undertones.";
-dairy.Smellyness = 4;
+
+
+List<Cheese> cheeses = new();
+
+cheeses.Add(new() {Kind = "Fourme d'Ambert", Taste = "Soft and creamy, mily with earthy undertones.", Smellyness = 4 });
+cheeses.Add(new() {Kind = "Langres", Taste = "Mild flavour, nutty with a certain saltyness, it has a long aftertaste.", Smellyness = 6 });
+cheeses.Add(new() {Kind = "Comté", Taste = "Semi-Hard, Strong tasting,Sweet and a little nutty", Smellyness = 2});
+
+
+
+// Cheese dairy = new Cheese();
+// dairy.Kind = "Fourme d'Ambert";
+// dairy.Taste = "Soft and creamy, milky with earthy undertones.";
+// dairy.Smellyness = 4;
 
 
 
@@ -14,7 +24,24 @@ dairy.Smellyness = 4;
 app.MapGet("/", Answer);
 
 app.MapGet("/cheese/", () => {
-return dairy;
+return cheeses;
+
+});
+
+app.MapGet("/Cheese/{num}", (int num) =>
+{
+
+if (num >= 0 && num < cheeses.Count)
+{
+return Results.Ok(cheeses[num]);
+}
+
+return Results.NotFound();
+});
+
+app.MapPost("/Cheese/", () => 
+{
+System.Console.WriteLine("POST!");
 
 });
 
